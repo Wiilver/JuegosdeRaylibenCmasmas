9a56bb93b1c099c9ecac2c5a9d18d4af4322ed05
Wiilver <susareywvh@gmail.com>
2026-06-28 00:52:41 -0600

    No se guardo

--- Banderas/main.cpp
+++ Banderas/main.cpp
@@ -1,52 +1,121 @@
 #include "raylib.h"
 
+struct Coor{
+    int x;
+    int y;
+};
+
+struct Direcciones{
+    bool arr = false;
+    bool aba = false;
+    bool izq = false;
+    bool der = false;
+};
+
+class Entidad{
+    public:
+        Coor pos;
+        Color color;
+
+        int rad;
+
+        void dibujar(){
+            DrawCircle(pos.x, pos.y, rad, color);
+        }
+};
+
+class Jugador : public Entidad{
+    public:
+        Direcciones dir;
+        
+        int vel = 10;
+        int puntuacion = 0;
+
+        void iniciarJugador(Coor &dim){
+            pos.x = dim.x/2;
+            pos.y = dim.y/2;
+            rad = 30;
+            color = WHITE;
+        }
+
+        void detectarDirecciones(){
+            dir.arr = (IsKeyDown(KEY_W)||IsKeyDown(KEY_UP));
+            dir.izq = (IsKeyDown(KEY_A)||IsKeyDown(KEY_LEFT));
+            dir.aba = (IsKeyDown(KEY_S)||IsKeyDown(KEY_DOWN));
+            dir.der = (IsKeyDown(KEY_D)||IsKeyDown(KEY_RIGHT));
+        }
+
+        void cambiarPosicion(Coor &dim){
+            if((dir.arr)&&(pos.y>rad)) pos.y-=vel;
+            if((dir.izq)&&(pos.x>rad)) pos.x-=vel;
+            if((dir.aba)&&(pos.y<dim.y-rad)) pos.y+=vel;
+            if((dir.der)&&(pos.x<dim.x-rad)) pos.x+=vel;
+        }
+};
+
+struct Bandera : public Entidad{
+    void iniciarBandera(){
+        pos.x = 0;
+        pos.y = 0;
+        rad = 35;
+        color = RED;
+    }
+
+    void generarPosicion(Coor &dim){
+        pos.x = GetRandomValue(int(dim.x/4), dim.x-rad);
+        pos.y = GetRandomValue(int(dim.y/4), dim.y-rad);
+
+        int rnd = GetRandomValue(0,3);
+
+        switch(rnd){
+            case 0:
+                return;
+            case 1:
+                pos.x*=-1;
+                return;
+            case 2:
+                pos.y*=-1;
+                pos.x*=-1;
+                return;
+            case 3:
+                pos.y*=-1;
+                return;
+        }
+    }
+
+};
+
 int main(){
-     const int ANCHO = 800;
-    const int ALTO  = 800;
-    InitWindow(ALTO, ANCHO, "Mi juego");
-    SetTargetFPS(60);
+    Coor dimensiones;
+    dimensiones.y = 800;
+    dimensiones.x = 800;
 
     const Color FONDO = {138, 199, 159, 1};
+    
+    Jugador jugador;
+    Bandera bandera;
 
-    bool arr, aba, izq, der;
-    arr = aba = izq = der = false;
-    
-    const int RAD = 30;
-    const int VEL = 50;
-    
-    int posx, posy;
-    posx = posy = 400;
+    jugador.iniciarJugador(dimensiones);
+    bandera.iniciarBandera();
+
+    bandera.generarPosicion(dimensiones);
+
+    InitWindow(dimensiones.y, dimensiones.x, "Mi juego");
+    SetTargetFPS(60);
 
     while(!WindowShouldClose()){
         //Manejar
 
-        arr = (IsKeyDown(KEY_UP)   ||IsKeyDown(KEY_W));
-        aba = (IsKeyDown(KEY_DOWN) ||IsKeyDown(KEY_S));
-        izq = (IsKeyDown(KEY_LEFT) ||IsKeyDown(KEY_A));
-        der = (IsKeyDown(KEY_RIGHT)||IsKeyDown(KEY_D));
-        
+        jugador.detectarDirecciones();
+
         //Actualizar
-        if(arr){
-            if(posy > VEL) posy -= VEL;  
-            else posy = RAD;
-        }
-        if(aba){
-            if(posy < (ALTO - VEL)) posy += VEL;  
-            else posy = ALTO - RAD;
-        }
-        if(izq){
-            if(posx > RAD) posx -= VEL;  
-            else posx=RAD;
-        }
-        if(der){
-            if(posx < (ANCHO - RAD)) posx += VEL;  
-            else posx = ANCHO - RAD;
-        }
+        jugador.cambiarPosicion(dimensiones);
 
         //Dibujar
         BeginDrawing();
         ClearBackground(FONDO);
-        DrawCircle(posx, posy, RAD, WHITE);
+        jugador.dibujar();
+        bandera.dibujar();
         EndDrawing();
     }
 
